da99085f1939b693747207f93f8d8fa471bfb755
KanizFatimaMitu <kanizfatima528@gmail.com>
2023-07-14 22:12:27 +0600

    style added

--- toWord.js
+++ toWord.js
@@ -4,6 +4,4 @@ let word = document.querySelector('h4');
 
 convert.addEventListener( 'click', () => {
     word.innerHTML = numberToWords.toWords(input.value);
-    word.innerHTML = numberToWords.toOrdinal(input.value);
-    word.innerHTML = numberToWords.toWordsOrdinal(input.value);
 } )
